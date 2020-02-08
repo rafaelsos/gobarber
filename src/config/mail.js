c6c8801278eab47fc62c11788942e27cc35e31c1
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-02-07 23:26:37 -0300

    feature: sendmail, jobs sendmail, queue, docker redis

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1017c3d50ae3ee',
+    pass: '048a4c865cf6b4',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
