1f6257374bf331b3cc31d7cd87f0d6a5073d6b4b
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-14 12:25:02 -0300

    web: criando e estiliznaod component Notificantions no Heade

--- webgobarber/src/components/Notifications/index.js
+++ webgobarber/src/components/Notifications/index.js
@@ -1,7 +1,56 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { MdNotifications } from 'react-icons/md';
+
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Scroll,
+  Notification,
+} from './styles';
 
 export default function Notifications() {
-  return <Container />;
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#7159c1" size={20} />
+      </Badge>
+
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>Voce possui um novo agendamento para amanhã</p>
+            <time>ha 2 dias </time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possui um novo agendamento para amanhã</p>
+            <time>ha 2 dias </time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possui um novo agendamento para amanhã</p>
+            <time>ha 2 dias </time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification unread>
+            <p>Voce possui um novo agendamento para amanhã</p>
+            <time>ha 2 dias </time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possui um novo agendamento para amanhã</p>
+            <time>ha 2 dias </time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possui um novo agendamento para amanhã</p>
+            <time>ha 2 dias </time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
 }
