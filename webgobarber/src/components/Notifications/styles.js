ea01342680beb47d5580380ec1c68e9999bca358
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-15 01:07:53 -0300

    web: ajustes styled

--- webgobarber/src/components/Notifications/styles.js
+++ webgobarber/src/components/Notifications/styles.js
@@ -31,7 +31,7 @@ export const NotificationList = styled.div`
   width: 260px;
   left: calc(50% - 130px);
   top: calc(100% + 30px);
-  background: rgba(0, 0, 0, 1);
+  background: rgba(0, 0, 0, 0.9);
   border-radius: 4px;
   padding: 15px 5px;
   display: ${props => (props.visible ? 'block' : 'none')};
@@ -44,7 +44,7 @@ export const NotificationList = styled.div`
     height: 0;
     border-left: 20px solid transparent;
     border-right: 20px solid transparent;
-    border-bottom: 20px solid rgba(0, 0, 0, 1);
+    border-bottom: 20px solid rgba(0, 0, 0, 0.9);
   }
 `;
 
--- webgobarber/src/pages/Dashboard/styles.js
+++ webgobarber/src/pages/Dashboard/styles.js
@@ -3,7 +3,7 @@ import { darken } from 'polished';
 
 export const Container = styled.div`
   max-width: 600px;
-  margin: 50px auto;
+  margin: 30px auto;
   display: flex;
   flex-direction: column;
   header {
