03b64cc2b49e3001a296ee6d9e0fcc7316f07338
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-22 22:16:28 -0300

    web: ajuste nos horarios possiveis para vizualizar

--- webgobarber/src/pages/Dashboard/index.js
+++ webgobarber/src/pages/Dashboard/index.js
@@ -18,7 +18,7 @@ import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
-const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 export default function Dashboard() {
   const [schedule, setSchedule] = useState([]);
